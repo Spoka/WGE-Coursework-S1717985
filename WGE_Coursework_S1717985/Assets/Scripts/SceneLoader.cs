d3c96efff70f014c14718cfd282d16757c39b7d2
Spoka <thekikogy@gmail.com>
2019-04-21 21:21:39 +0100

    .

---
+++ WGE_Coursework_S1717985/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadScene1()
+    {
+        Application.LoadLevel("Scene 1");
+    }
+
+    public void LoadScene2()
+    {
+        Application.LoadLevel("Scene 2");
+    }
+}
